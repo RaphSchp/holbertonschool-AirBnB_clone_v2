de1a30aa9a1d542f2556c7eb5df35b311eae185b
Groinkb <benjamin@mossu.ch>
2023-12-01 21:28:31 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from os import getenv
+""" This module defines a class User """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, null
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    storage_engine = getenv('HBNB_TYPE_STORAGE')
-    if storage_engine is None:
-        storage_engine = "db"
-
-    if storage_engine == "db":
-        __tablename__ = 'users'
-
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True, default=null())
-        last_name = Column(String(128), nullable=True, default=null())
-        places = relationship("Place", back_populates="user",
-                              cascade="delete, delete-orphan")
-        reviews = relationship("Review", back_populates="user",
-                               cascade="delete, delete-orphan")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
-        
+    """ This class defines a user by various attributes
+        Attributs:
+        ====================
+            email : email of user
+                    String, not null
+            password: password of compte user
+                    String, not null
+            first_name : name of user
+                    String, not null
+            last_name: last name of user
+                    String, not null
+    """
+    __tablename__ = "users"
+    email = Column(
+        String(128),
+        nullable=False)
+    password = Column(
+        String(128),
+        nullable=False
+    )
+    first_name = Column(
+        String(128),
+        nullable=True,
+    )
+    last_name = Column(
+        String(128),
+        nullable=True,
+    )
+    places = relationship(
+        "Place",
+        backref="user")
+    reviews = relationship(
+        "Review",
+        backref="user")
