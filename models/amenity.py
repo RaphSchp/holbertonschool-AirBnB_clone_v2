4591f8d2fa4090499be90efc127da2103f4d6cca
Groinkb <benjamin@mossu.ch>
2023-12-01 21:17:07 +0100

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -1,31 +1,21 @@
-#!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from os import getenv
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey, Table
 
 
 class Amenity(BaseModel, Base):
-    """ Review class to store review information """
-    storage_engine = getenv('HBNB_TYPE_STORAGE')
-    if storage_engine is None:
-        storage_engine = "db"
-
-    if storage_engine == "db":
+    """Representation of Amenity """
+    if models.storage_t == 'db':
         __tablename__ = 'amenities'
-
-        place_amenity = Table(
-            'place_amenity',
-            Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'),
-                nullable=False, primary_key=True),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                nullable=False, primary_key=True),
-            extend_existing=True
-        )
-
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity)
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
