6b5439d9e7a770b9b3734f94ececedbf7b78289c
Groinkb <benjamin@mossu.ch>
2023-12-01 17:52:26 +0100

    add  user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, null
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    storage_engine = getenv('HBNB_TYPE_STORAGE')
+    if storage_engine is None:
+        storage_engine = "db"
+
+    if storage_engine == "db":
+        __tablename__ = 'users'
+
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True, default=null())
+        last_name = Column(String(128), nullable=True, default=null())
+        places = relationship("Place", back_populates="user",
+                              cascade="delete, delete-orphan")
+        reviews = relationship("Review", back_populates="user",
+                               cascade="delete, delete-orphan")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
