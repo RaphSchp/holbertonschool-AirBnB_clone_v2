1176196eb866ff8647d9bf92191607c3063f6394
RaphSchp <135048201+RaphSchp@users.noreply.github.com>
2023-12-15 16:34:25 +0000

    add doc to module

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-''' flask setup'''
+''' 
+This is a Flask web application for handling states and cities.
+
+It includes routes for displaying a list of states and information about a specific state.
+
+Author: Your Name
+'''
+
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -10,26 +17,25 @@ from models.engine.db_storage import DBStorage
 
 app = Flask(__name__)
 
-
 @app.route('/states', strict_slashes=False)
 def state_ls():
+    '''Displays a list of all states.'''
     states = storage.all("State").values()
     return render_template('9-states.html', states=states, mode='all')
 
-
 @app.route('/states/<id>', strict_slashes=False)
 def state_id(id):
+    '''Displays information about a specific state based on the provided ID.'''
     states = storage.all("State").values()
     for state in states:
         if state.id == id:
             return render_template('9-states.html', states=state, mode='id')
     return render_template('9-states.html', states=state, mode=';')
 
-
 @app.teardown_appcontext
 def storage_close(self):
+    '''Closes the storage connection after each request.'''
     storage.close()
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
